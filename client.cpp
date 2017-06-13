4c144229d9fa1881995a2543728b5aa8170ea03c
theworld0828 <theworld0828@163.com>
2017-06-13 22:45:35 +0800

    add some function

--- client.cpp
+++ client.cpp
@@ -1,2 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include "reactor.h"
+
+
+
+class client :public event_handler{
+public:
+	client();
+	~client();
+
+	int connet_server()
+	{
+
+	}
+	void  read_event()
+	{
+
+	}
+	void write_event()
+	{
+
+	}
+
+	handle_t get_handle()
+	{
+		return handle;
+	}
+
+
+private:
+	handle_t handle;
+};
+
+int main(arg,argv)
+{
+
+}
+
+
+
